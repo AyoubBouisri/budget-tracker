334ae7c68d4c01fe2b7525fe9ced0b29bb43da09
Ayoub <ayoubbousri@hotmail.fr>
2022-05-02 21:33:47 -0400

    Parses input in an infinite loop + Script ideas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
+
+// Script to deal with expenses
+//
+//
+// 1. Log
+//      name : str
+//      price: int / float
+//      categorie: str
+//      why? : str optional
+//      reccurence? = one time | [monthly, yearly, biweekly etc]
+//          -> Saves everything in a tight format to a text file
+//
+// 2. List 
+//      show by two dates [start, end] 
+//      show by month
+//      show by week
+//
+// 3. Analyse
+//      analyse by two dates
+//      analyse by month
+//      analyse by week
+//          ---- Sum everything
+//          ---- Sum by Categorie
+//      
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    loop {
+        println!("What did you buy today ?");
+        io::stdin().read_line(&mut input).expect("Error reading input");
+        println!("{}", input);
+    }
+
 }
+
+
